43740b377e0dc0d5ac54bc433b64785ce3c965bd
Tudor Hulban <prindex2010@gmail.com>
2025-05-22 17:53:21 +0300

    m1 benchmark

--- benchmark_test.go
+++ benchmark_test.go
@@ -12,6 +12,9 @@ import (
 // with all values precomputed
 // cpu: AMD Ryzen 7 5800H with Radeon Graphics
 // BenchmarkNewIDSimple-16    	10265635	       115.8 ns/op	      24 B/op	       1 allocs/op
+
+// cpu: Apple M1
+// BenchmarkNewIDSimple-8   	11679970	       104.1 ns/op	      24 B/op	       1 allocs/op
 func BenchmarkNewIDSimple(b *testing.B) {
 	generator := NewEpochGenerator()
 
